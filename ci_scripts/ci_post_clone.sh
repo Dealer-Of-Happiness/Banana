8ebb57902d1e1431a9a3a854fb29abe95bad384f
DealerOfHappiness <marketing@dealerofhappiness.com>
2026-01-11 15:46:43 -0500

    Fix: Use skipMacroValidation for Swift macros in Xcode Cloud

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-# Trust Swift macros for Xcode Cloud
-defaults write com.apple.dt.Xcode IDESkipMacroFingerprintValidation -bool YES
+set -e
+
+# Pre-resolve packages with macro validation skipped
+xcodebuild -resolvePackageDependencies \
+  -project "$CI_WORKSPACE/AIGoodbye/AIGoodbye.xcodeproj" \
+  -scheme AIGoodbye \
+  -skipMacroValidation
